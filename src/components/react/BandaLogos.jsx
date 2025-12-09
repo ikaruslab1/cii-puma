f7cefd4b36c31298c3c17eee64faf64cdfb87f0c
ikaruslab1 <ikarusgdm@gmail.com>
2025-12-08 18:56:34 -0600

    [FIX / ADD] comite-comitte / logo UIMA

--- src/components/react/BandaLogos.jsx
+++ src/components/react/BandaLogos.jsx
@@ -3,9 +3,9 @@ import LogoLoop from './LogoLoop';
 const imageLogos = [
     { src: "https://nyc.cloud.appwrite.io/v1/storage/buckets/6930504d0026f8982f8e/files/693053fb0002cb0492dd/view?project=69304e09002505d69656&mode=admin", alt: "UNAM", href: "https://www.unam.mx/" },
     { src: "https://nyc.cloud.appwrite.io/v1/storage/buckets/6930504d0026f8982f8e/files/693053eb003b5848fa97/view?project=69304e09002505d69656&mode=admin", alt: "FESA", href: "https://www.unam.mx/" },
+    { src: "https://nyc.cloud.appwrite.io/v1/storage/buckets/6930504d0026f8982f8e/files/693772d40011620ee15a/view?project=69304e09002505d69656&mode=admin", alt: "UIMA" },
     { src: "https://nyc.cloud.appwrite.io/v1/storage/buckets/6930504d0026f8982f8e/files/693056bd0013bcdd3f4a/view?project=69304e09002505d69656&mode=admin", alt: "Qaldas", href: "https://qaldas.com/" },
     { src: "https://nyc.cloud.appwrite.io/v1/storage/buckets/6930504d0026f8982f8e/files/693056c2000d39dd75df/view?project=69304e09002505d69656&mode=admin", alt: "QMéxico", href: "https://qmexico.org/" },
-
 ];
 
 function BandaLogos() {
